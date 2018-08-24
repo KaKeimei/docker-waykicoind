fe320dce6a89a984fcf4c9c1e85aacc662f6c060
DragonMaster <tigerich@users.noreply.github.com>
2018-08-24 13:40:48 +0800

    Update run-waykicoind.sh
    mount host directories and files into docker container

--- bin/run-waykicoind.sh
+++ bin/run-waykicoind.sh
@@ -1,1 +1,4 @@
-docker run --name waykicoind-test --rm -p6968:6968 -d waykichain/wcoind:1.0
+docker run --name waykicoind-test --rm -p6968:6968 \
+       -v `pwd`/conf/WaykiChain.conf:/wicc/WaykiChain.conf \
+       -v `pwd`/data:/wicc/testnet \
+       -d waykichain/wcoind:1.0
