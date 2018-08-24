39e35e18686dd1fbf706d7c7e0e3c86411b3c747
Richard Chen <richardchen@Richards-MacBook-Pro.local>
2018-08-24 14:46:34 +0800

    add start main sh

--- bin/run-waykicoind-main.sh
+++ bin/run-waykicoind-main.sh
@@ -1,4 +1,4 @@
-docker run --name waykicoind-test --rm -p6968:6968 \
+docker run --name waykicoind --rm -p6968:6968 \
        -v `pwd`/conf/WaykiChain.conf:/wicc/WaykiChain.conf \
-       -v `pwd`/data:/wicc/testnet \
+       -v `pwd`/data:/wicc/main \
        -d waykichain/wcoind:1.0
