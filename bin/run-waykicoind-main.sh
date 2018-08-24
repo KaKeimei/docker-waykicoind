26bb4a1c7876c00123a981a575a85069f9fa9d8b
Dragon Master <tigerich@users.noreply.github.com>
2018-08-24 17:05:12 +0800

    Update run-waykicoind-main.sh
    update format

--- bin/run-waykicoind-main.sh
+++ bin/run-waykicoind-main.sh
@@ -1,4 +1,4 @@
-docker run --name waykicoind-main --rm -p6968:6968 \
+docker run --name waykicoind-main --rm -p 6968:6968 \
        -v `pwd`/conf/WaykiChain.conf:/wicc/WaykiChain.conf \
        -v `pwd`/data:/wicc/main \
        -d waykichain/wcoind:1.0
